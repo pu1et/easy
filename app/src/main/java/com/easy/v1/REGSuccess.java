68a903d1eab9fe6dc842e8251d643a509f35bf94
pu1et <pu1etproof@gmail.com>
2021-01-26 23:25:30 +0900

    test

--- app/src/main/java/com/easy/v1/REGSuccess.java
+++ app/src/main/java/com/easy/v1/REGSuccess.java
@@ -29,16 +29,7 @@ public class REGSuccess extends AppCompatActivity {
                     Intent intent = new Intent(REGSuccess.this, MainActivity.class);
                     startActivity(intent);
                     break;
-                    /*
-                    case R.id.btn_tomain:
-                    Intent intent = new Intent(REGSuccess.this, MainActivity.class);
-                    startActivity(intent);
-                    break;
-                    case R.id.btn_tomain:
-                    Intent intent = new Intent(REGSuccess.this, MainActivity.class);
-                    startActivity(intent);
-                    break;
-                    */
+
             }
         }
     };
